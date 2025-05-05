3b391bfb835dfaf38806defaec5068472e119b63
pico <nicopeuser5@gmail.com>
2025-05-05 17:39:57 +0200

    Little change, removed impoort statement

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from flask_cors import CORS  # Importiere CORS
+from flask_cors import CORS 
 
 from .config import Config
 
